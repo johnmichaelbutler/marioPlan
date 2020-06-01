9dbf7b288c2c09d5b13f5580ed5ae8ce5ddbebda
John Butler <johnmichaelbutler@yahoo.com>
2020-06-01 16:54:10 -0500

    Add data to firestore using create_project action

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,11 +2,22 @@ export const createProject = (project) => {
   // Returns a function for redux thunk
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // Make async call to database
-
-    // Then dispatch the action
-    dispatch({
-      type: 'CREATE_PROJECT',
-      project
-    });
+    const firestore = getFirestore();
+    // Thunk returns a promise
+    firestore.collection('projects').add({
+      ...project,
+      authorFirstName: 'John',
+      authorLastName: 'Butler',
+      authorId: 123,
+      createdAt: new Date()
+    }).then(() => {
+      // Then dispatch the action
+      dispatch({
+        type: 'CREATE_PROJECT',
+        project
+      });
+    }).catch((err) => {
+      dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+    })
   }
 };
