8e601648bcb90a29e18e01b39e903f02e84c5446
John Butler <johnmichaelbutler@yahoo.com>
2020-06-01 15:34:05 -0500

    Add dummy data to fill project summary. A setup for redux

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
-import Notifications from './components/dashboard/Notifications';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
