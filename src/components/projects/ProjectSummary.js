f5e1c184552451fa230e4d8eee5f888cbee814d5
John Butler <johnmichaelbutler@yahoo.com>
2020-06-05 10:00:08 -0500

    Format our dates with moment

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import moment from 'moment';
 
 const ProjectSummary = ({ project }) => {
   return (
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
-  <p>Created By: {project.authorFirstName} {project.authorLastname}</p>
-        <p className="grey-text">Today</p>
+        <p>Created By: {project.authorFirstName} {project.authorLastname}</p>
+        <p className="grey-text">{moment(project.createdAt.toDate().toString()).calendar()}</p>
       </div>
     </div>
   )
