8a083e3b52d5d4e0b545f5ca4d125082406c4b3a
John Butler <johnmichaelbutler@yahoo.com>
2020-06-05 11:37:40 -0500

    Deployed to production

---
+++ dist/precache-manifest.52153ff46620df207f63004a874a19b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b871661280839d04d75b9e610bb2f5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c106d0304567ec91f50f",
+    "url": "/static/css/main.1661e4e8.chunk.css"
+  },
+  {
+    "revision": "9c13a7d9321dba9cfae2",
+    "url": "/static/js/2.07881877.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.07881877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c106d0304567ec91f50f",
+    "url": "/static/js/main.312fe1c9.chunk.js"
+  },
+  {
+    "revision": "98a629c2f8b7a0ef59a4",
+    "url": "/static/js/runtime-main.0d165b64.js"
+  }
+]);
