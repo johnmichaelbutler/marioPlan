a169aa2285fe37382eb1c5bbbe5f8fe5147fc895
John Butler <johnmichaelbutler@yahoo.com>
2020-06-05 08:06:41 -0500

    Imported a Spinner component from a previous project. Added the AuthIsLoaded component to our App.js which will render our Spinner if firebase is still loading.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { isLoaded } from 'react-redux-firebase';
+
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import CreateProject from './components/projects/CreateProject';
+import Spinner from './components/auth/Spinner';
+
+function AuthIsLoaded({ children }) {
+  const auth = useSelector(state => state.firebase.auth)
+  if (!isLoaded(auth)) return <Spinner />;
+  return children
+};
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route path='/project/:id' component={ProjectDetails} />
-          <Route path='/signin' component={SignIn} />
-          <Route path='/signup' component={SignUp} />
-          <Route path='/create' component={CreateProject} />
+      <AuthIsLoaded>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route path='/project/:id' component={ProjectDetails} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
+            <Route path='/create' component={CreateProject} />
 
-          <Route exact path='/' component={Dashboard} />
-        </Switch>
-      </div>
-
-
+            <Route exact path='/' component={Dashboard} />
+          </Switch>
+        </div>
+      </AuthIsLoaded>
     </BrowserRouter>
   );
 }
